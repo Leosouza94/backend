f765247a1dc8f1c30a0b450fce7fa49a0c3eef27
Leosouza94 <leosouza94@hotmail.com>
2025-08-20 12:59:48 -0300

    arquivos 3

--- dist/ormconfig.js
+++ dist/ormconfig.js
@@ -1,17 +1,22 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppDataSource = void 0;
 const typeorm_1 = require("typeorm");
 const Exam_1 = require("./entities/Exam");
 const User_1 = require("./entities/User");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config(); // carrega variáveis do .env
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "", //"IFuP95ibs1hICIF",
-    database: "saudebarceloserios",
-    synchronize: false, // para criar tabelas automaticamente (só em dev)
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    synchronize: false,
     logging: false,
     entities: [User_1.User, Exam_1.Exam],
 });
